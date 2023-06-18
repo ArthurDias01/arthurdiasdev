75afb3a926fbe08485ce11671b5958b36e47387c
Arthur Dias <arthursantos01@gmail.com>
2023-06-18 14:16:23 -0300

    fix: aspect ratio project card + classes

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -17,21 +17,22 @@ export const ProjectCard = ({
   id,
 }: Props) => {
   return (
-    <div className="flex h-full max-h-[16rem] w-full max-w-sm flex-col gap-4 overflow-hidden rounded-[20px] bg-neutral-500 duration-300 dark:bg-neutral-900">
-      <div className="max-h-[150px] w-full flex-1 overflow-hidden bg-red-400">
-        <Link href={`/project/${id}`}>
-          <Image
-            src={imageSrc}
-            alt={imageAlt}
-            width={340}
-            height={240}
-            className="aspect-video h-auto min-h-[150px] w-full transform cursor-pointer object-cover transition duration-200 ease-in-out hover:scale-110"
-          />
-        </Link>
-      </div>
+    <div className="flex aspect-square h-full max-h-[16rem] w-full max-w-sm animate-scaleUp flex-col gap-2 overflow-hidden rounded-[20px] bg-neutral-500 duration-300 dark:bg-neutral-900">
+      <Link
+        href={`/project/${id}`}
+        className="max-h-3/4 h-full w-full overflow-hidden"
+      >
+        <Image
+          src={imageSrc}
+          alt={imageAlt}
+          width={340}
+          height={240}
+          className="relative h-full w-full transform cursor-pointer object-cover transition duration-300 ease-in-out hover:scale-110"
+        />
+      </Link>
 
-      <div className="flex h-1/4 flex-col justify-center truncate px-4 pb-4">
-        <h3 className="text-md text-neutral-200 dark:text-neutral-300">
+      <div className="flex h-1/3 w-full flex-col truncate px-4 pb-4">
+        <h3 className="text-sm text-neutral-200 dark:text-neutral-300">
           {category}
         </h3>
         <h2 className="text-lg font-semibold text-primary-400">{title}</h2>
