852627ca89c41e28109fe47b57e6609494b9fc48
Arthur Dias <arthursantos01@gmail.com>
2023-06-18 12:31:09 -0300

    fix: projectcard aspect ration and wrapping on mobile

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -17,7 +17,7 @@ export const ProjectCard = ({
   id,
 }: Props) => {
   return (
-    <div className="flex h-full w-full max-w-md flex-1 animate-scaleUp flex-col   justify-center overflow-hidden rounded-[20px] bg-neutral-500 duration-300 dark:bg-neutral-900">
+    <div className="flex h-full w-full max-w-md animate-scaleUp  flex-col gap-4 overflow-hidden rounded-[20px] bg-neutral-500 duration-300 dark:bg-neutral-900">
       <div className="h-full w-full overflow-hidden">
         <Link href={`/project/${id}`}>
           <Image
@@ -25,15 +25,15 @@ export const ProjectCard = ({
             alt={imageAlt}
             width={340}
             height={240}
-            className="aspect-video min-h-[240px] w-full transform cursor-pointer object-cover transition duration-200 ease-in-out hover:scale-110"
+            className="aspect-video h-auto min-h-[150px] w-full transform cursor-pointer object-cover transition duration-200 ease-in-out hover:scale-110"
           />
         </Link>
       </div>
-      <div className="flex max-h-[33%] flex-col justify-center truncate px-4 pb-4">
+      <div className="flex h-fit flex-col justify-center px-4 pb-4">
         <h3 className="text-md text-neutral-200 dark:text-neutral-300">
           {category}
         </h3>
-        <h2 className="text-lg font-medium text-primary-400">{title}</h2>
+        <h2 className="text-lg font-semibold text-primary-400">{title}</h2>
       </div>
     </div>
   )
