473d5aa7054de316777e544e33bd677067f56ad5
Arthur Dias <arthursantos01@gmail.com>
2023-07-29 16:02:24 -0300

    fix: get-resume route deleted

--- src/app/api/get-resume-file/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-import { getResume } from '../../../lib/contentapi'
-
-export async function GET() {
-  const data = await getResume()
-
-  return NextResponse.json({ data })
-}
