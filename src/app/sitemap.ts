d0b90afe3902957cce1bf5566234b5b77ce54460
Arthur Dias <arthursantos01@gmail.com>
2024-01-11 14:22:40 -0300

    fix to the sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,29 +1,53 @@
 import { getProjects } from '../lib/contentapi'
 
-export default async function sitemap() {
+type Sitemap = Array<{
+  url: string
+  lastModified?: string | Date
+  changeFrequency?:
+    | 'always'
+    | 'hourly'
+    | 'daily'
+    | 'weekly'
+    | 'monthly'
+    | 'yearly'
+    | 'never'
+  priority?: number
+}>
+
+export default async function sitemap(): Promise<Sitemap> {
   const projects = await getProjects()
   const projectUrls = projects.map((project) => {
     return {
       url: `https://arthurdias.dev/projects/${project.id}`,
-      lastModified: new Date(project.date),
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
     }
-  })
+  }) as Sitemap
   return [
     {
       url: 'https://arthurdias.dev',
       lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
     },
     {
       url: 'https://arthurdias.dev/about',
       lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.95,
     },
     {
       url: 'https://arthurdias.dev/projects',
       lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.95,
     },
     {
       url: 'https://arthurdias.dev/contact',
       lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.95,
     },
     ...projectUrls,
   ]
