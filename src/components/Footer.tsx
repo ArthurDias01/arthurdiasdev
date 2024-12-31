6081d16b82d1bf9b7a0f384be5a639970f336981
ArthurDias01 <arthursantos01@gmail.com>
2024-12-31 18:50:11 -0500

    Update footer to display the current year dynamically instead of a hardcoded value

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <div className="item-center mt-16 flex w-full flex-row flex-wrap justify-center bg-primary-500 font-semibold text-white">
-      © 2023 All Rights Reserved by Arthur Dias.
+      © {new Date().getFullYear()} All Rights Reserved by Arthur Dias.
     </div>
   )
 }
